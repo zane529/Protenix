2886542d2726cbf432f2439bbdb47587e505169a
yangyanpinghpc <yangyanping.hpc@bytedance.com>
2024-11-14 17:41:44 +0800

    remove layernorm chunk code which will make train/inference slow down

--- protenix/model/layer_norm/layer_norm.py
+++ protenix/model/layer_norm/layer_norm.py
@@ -126,28 +126,7 @@ class FusedLayerNorm(torch.nn.Module):
         torch.nn.init.zeros_(self.bias)
 
     def forward(self, input):
-        if len(input.shape) >= 3 and input.shape[-3] > 6000:
-            out = torch.empty_like(input)
-            # Set max chunk_size = dim / 2, to max compute efficiency
-            chunk_size = min(4000 * 4000 // input.shape[-3], (input.shape[-3] + 1) // 2)
-            if len(input.shape) == 3:
-                for i in range(input.shape[-3]):
-                    out[i : i + chunk_size] = self.kernel_forward(
-                        input[i : i + chunk_size]
-                    )
-            elif len(input.shape) == 4:
-                for j in range(input.shape[-4]):
-                    for i in range(0, input.shape[-3], chunk_size):
-                        out[j, i : i + chunk_size] = self.kernel_forward(
-                            input[j, i : i + chunk_size]
-                        )
-            else:
-                raise RuntimeError(
-                    "Shape" + input.shape + "not implemented for layernorm yet!"
-                )
-            return out
-        else:
-            return self.kernel_forward(input)
+        return self.kernel_forward(input)
 
     def kernel_forward(self, input):
         return FusedLayerNormAffineFunction.apply(
